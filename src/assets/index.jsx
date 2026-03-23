09aa4d57e4e6ba77d5e5115b176b9bb60039d4f5
Arnaud <bassolearnaud9@gmail.com>
2026-03-23 15:08:43 +0000

    l

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,57 +1,57 @@
-export { default as image1 } from "./image/floue.jpg";
-export { default as image2 } from "./image/logo_nci.png";
-export { default as image3 } from "./image/dailymotion_logo_icon_147302.webp";
-export { default as image4 } from "./image/insta.png";
-export { default as image5 } from "./image/fb.png";
-export { default as image6 } from "./image/tiktok_logo_icon_186896.png";
-export { default as image7 } from "./image/twitter.png";
-export { default as image8 } from "./image/you.png";
-export { default as image9 } from "./image/social_media_logo_google_icon-icons.com_59074.png";
-export { default as image10 } from "./image/social_apple_mac_25.png";
-export { default as image11 } from "./image/user.svg";
+export { default as image1 } from "../public/image/floue.jpg";
+export { default as image2 } from "../public/image/logo_nci.png";
+export { default as image3 } from "../public/image/dailymotion_logo_icon_147302.webp";
+export { default as image4 } from "../public/image/insta.png";
+export { default as image5 } from "../public/image/fb.png";
+export { default as image6 } from "../public/image/tiktok_logo_icon_186896.png";
+export { default as image7 } from "../public/image/twitter.png";
+export { default as image8 } from "../public/image/you.png";
+export { default as image9 } from "../public/image/social_media_logo_google_icon-icons.com_59074.png";
+export { default as image10 } from "../public/image/social_apple_mac_25.png";
+export { default as image11 } from "../public/image/user.svg";
 
 
-export { default as image13 } from "./image/shop2.jpg";
-export { default as image14 } from "./image/blousson.png";
-export { default as image15 } from "./image/chemise.png";
-export { default as image16 } from "./image/tee-short.png";
-export { default as image17 } from "./image/debardeur.png";
-export { default as image18 } from "./image/polo.jpg";
-export { default as image19 } from "./image/blouson-ski-femme.png";
-
-
-
-export { default as image20 } from "./image/adidas.pnd.jpeg";
-export { default as image21 } from "./image/jordan2.jpg";
-export { default as image22 } from "./image/jordane1.png";
-export { default as image23 } from "./image/nike1.png";
-export { default as image24 } from "./image/nike2.png";
-export { default as image25 } from "./image/yeezy.png";
-
-
-export { default as image26 } from "./image/huawei.png";
-export { default as image27 } from "./image/iPhone15.png";
-export { default as image28 } from "./image/iphone16.jpeg";
-export { default as image29 } from "./image/s23ultra.png";
-export { default as image30 } from "./image/samsung_s24.png";
-export { default as image31 } from "./image/sony.png";
+export { default as image13 } from "../public/image/shop2.jpg";
+export { default as image14 } from "../public/image/blousson.png";
+export { default as image15 } from "../public/image/chemise.png";
+export { default as image16 } from "../public/image/tee-short.png";
+export { default as image17 } from "../public/image/debardeur.png";
+export { default as image18 } from "../public/image/polo.jpg";
+export { default as image19 } from "../public/image/blouson-ski-femme.png";
 
 
 
+export { default as image20 } from "../public/image/adidas.pnd.jpeg";
+export { default as image21 } from "../public/image/jordan2.jpg";
+export { default as image22 } from "../public/image/jordane1.png";
+export { default as image23 } from "../public/image/nike1.png";
+export { default as image24 } from "../public/image/nike2.png";
+export { default as image25 } from "../public/image/yeezy.png";
 
-export { default as image32 } from "./image/tiktok_logo.png";
-export { default as image33 } from "./image/fb.png";
-export { default as image34 } from "./image/youtube.png";
-export { default as image35 } from "./image/google.png";
-export { default as image36 } from "./image/insta.png";
-export { default as image37 } from "./image/humain.png";
-export { default as image38 } from "./image/humain2.png";
-export { default as image39 } from "./image/humain3.png";
-export { default as image40 } from "./image/blob.svg";
-export { default as image41 } from "./image/shop.jpg";
-export { default as image42 } from "./image/vetement.png";
-export { default as image43 } from "./image/pair.png";
-export { default as image44 } from "./image/vetem.png";
-export { default as image45 } from "./image/log.png";
-export { default as image46 } from "./image/visage.png";
-export { default as image47 } from "./image/visage2.png";
+
+export { default as image26 } from "../public/image/huawei.png";
+export { default as image27 } from "../public/image/iPhone15.png";
+export { default as image28 } from "../public/image/iphone16.jpeg";
+export { default as image29 } from "../public/image/s23ultra.png";
+export { default as image30 } from "../public/image/samsung_s24.png";
+export { default as image31 } from "../public/image/sony.png";
+
+
+
+
+export { default as image32 } from "../public/image/tiktok_logo.png";
+export { default as image33 } from "../public/image/fb.png";
+export { default as image34 } from "../public/image/youtube.png";
+export { default as image35 } from "../public/image/google.png";
+export { default as image36 } from "../public/image/insta.png";
+export { default as image37 } from "../public/image/humain.png";
+export { default as image38 } from "../public/image/humain2.png";
+export { default as image39 } from "../public/image/humain3.png";
+export { default as image40 } from "../public/image/blob.svg";
+export { default as image41 } from "../public/image/shop.jpg";
+export { default as image42 } from "../public/image/vetement.png";
+export { default as image43 } from "../public/image/pair.png";
+export { default as image44 } from "../public/image/vetem.png";
+export { default as image45 } from "../public/image/log.png";
+export { default as image46 } from "../public/image/visage.png";
+export { default as image47 } from "../public/image/visage2.png";
